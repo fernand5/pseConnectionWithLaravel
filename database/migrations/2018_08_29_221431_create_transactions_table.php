22914e78c8fd86067cbcbf81520e0e37ece28c5c
Fernando Fernandez <feralfer.5@gmail.com>
2018-08-30 09:42:08 -0500

    - Transaction controller created with its resource

--- database/migrations/2018_08_29_221431_create_transactions_table.php
+++ database/migrations/2018_08_29_221431_create_transactions_table.php
@@ -13,8 +13,12 @@ class CreateTransactionsTable extends Migration
      */
     public function up()
     {
+
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('make');
+            $table->string('model');
+            $table->date('produced_on');
             $table->timestamps();
         });
     }
